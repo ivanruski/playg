a5faa77310952acb5625fe1c60b736dde017802b
Ivan Ivanov <ivan.ivanov@ft.com>
2022-06-11 18:30:19 +0300

    ctci: Trees and Graphs: Check Subtree v2
    This solution should be faster because we are only comparing the subtrees with
    the same height.

--- ctci/ch04/10/main.go
+++ ctci/ch04/10/main.go
@@ -20,15 +20,17 @@ func main() {
 }
 
 func isSubtree(root *TreeNode, subRoot *TreeNode) bool {
-	if root == nil {
-		return false
+	subRootHeight := treeHeight(subRoot)
+	nodesToCheck := []*TreeNode{}
+	nodesWithHeight(root, subRootHeight, &nodesToCheck)
+
+	for _, n := range nodesToCheck {
+		if areTreesEqual(n, subRoot) {
+			return true
+		}
 	}
 
-	if areTreesEqual(root, subRoot) {
-		return true
-	}
-
-	return isSubtree(root.Left, subRoot) || isSubtree(root.Right, subRoot)
+	return false
 }
 
 func areTreesEqual(x, y *TreeNode) bool {
@@ -44,4 +46,41 @@ func areTreesEqual(x, y *TreeNode) bool {
 	}
 
 	return areTreesEqual(x.Left, y.Left) && areTreesEqual(x.Right, y.Right)
+}
+
+func treeHeight(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+
+	lh := treeHeight(root.Left)
+	rh := treeHeight(root.Right)
+
+	if lh > rh {
+		return lh + 1
+	}
+	return rh + 1
+}
+
+func nodesWithHeight(root *TreeNode, height int, nodes *[]*TreeNode) int {
+	if root == nil {
+		return 0
+	}
+
+	lh := nodesWithHeight(root.Left, height, nodes)
+	rh := nodesWithHeight(root.Right, height, nodes)
+	var nodeHeight int
+	if lh > rh {
+		nodeHeight = lh + 1
+	} else {
+		nodeHeight = rh + 1
+	}
+
+	if height == nodeHeight {
+		ns := *nodes
+		ns = append(ns, root)
+		*nodes = ns
+	}
+
+	return nodeHeight
 }
