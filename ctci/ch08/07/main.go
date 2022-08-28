c045e15bd45df7fa3fefed63755e488842c9bc5f
Ivan Ivanov <ivan.ivanov@ft.com>
2022-08-28 17:19:51 +0300

    ctci: Recursion and DP - Permutations without Dups - 2
    Implement another approach

--- ctci/ch08/07/main.go
+++ ctci/ch08/07/main.go
@@ -8,10 +8,12 @@
 
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	arr := []int{1, 2, 3}
+	arr := []int{1, 2, 3, 4}
 	perms := permute(arr)
 	for i := 0; i < len(perms); i++ {
 		fmt.Printf("%v\n", perms[i])
@@ -19,26 +21,60 @@ func main() {
 }
 
 func permute(nums []int) [][]int {
+	return permute1(nums)
+	//var (
+	//	permutations = make([][]int, 0, factorial(len(nums)))
+	//	prefix       = make([]int, 0, len(nums))
+	//)
+	//
+	//permute2(nums, prefix, &permutations)
+	//return permutations
+}
+
+func permute1(nums []int) [][]int {
 	if len(nums) == 1 {
 		return [][]int{
 			{nums[0]},
 		}
 	}
 
-	result := make([][]int, 0, factorial(len(nums)))
+	ps := make([][]int, 0, factorial(len(nums)))
 	suffix := make([]int, len(nums)-1)
 	for i := 0; i < len(nums); i++ {
 		copy(suffix, nums[:i])
 		for j := i + 1; j < len(nums); j++ {
 			suffix[j-1] = nums[j]
 		}
-		sp := permute(suffix)
-		for j := 0; j < len(sp); j++ {
-			result = append(result, append(sp[j], nums[i]))
+		tmps := permute1(suffix)
+		for j := 0; j < len(tmps); j++ {
+			ps = append(ps, append(tmps[j], nums[i]))
 		}
 	}
+	return ps
+}
 
-	return result
+func permute2(nums, prefix []int, permutations *[][]int) {
+	if len(prefix) == len(nums) {
+		p := make([]int, len(nums))
+		copy(p, prefix)
+		*permutations = append(*permutations, p)
+		return
+	}
+
+	for _, n := range nums {
+		if !sliceContains(prefix, n) {
+			permute2(nums, append(prefix, n), permutations)
+		}
+	}
+}
+
+func sliceContains(nums []int, n int) bool {
+	for i := 0; i < len(nums); i++ {
+		if nums[i] == n {
+			return true
+		}
+	}
+	return false
 }
 
 func factorial(n int) int {
