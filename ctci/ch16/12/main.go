d247b0e02a6783488d27dff1cbe46147c3dbcd02
Ivan Ivanov <ruski9606@gmail.com>
2022-11-23 19:26:25 +0200

    ctci: Moderate - XML Encoding
    Solution heavily inspired by https://go.dev/talks/2011/lex.slide#1

--- ctci/ch16/12/main.go
+++ ctci/ch16/12/main.go
@@ -23,21 +23,18 @@
 package main
 
 import (
-	"errors"
 	"fmt"
-	"io"
+	"os"
 	"strconv"
 	"strings"
 	"unicode"
+	"unicode/utf8"
 )
 
 func main() {
-	doc := `<note>
-	  <to>Tove</to>
-	  <from>Jani</from>
-	  <heading>Reminder</heading>
-	  <body>Don't forget me this weekend!</body>
-	</note>`
+	doc := `<family lastName="McDowell" state="CA">
+  <person firstName="Gayle"> Some Message</person>
+</family>`
 
 	mapping := map[string]int{
 		"family":    1,
@@ -47,261 +44,283 @@ func main() {
 		"state":     5,
 	}
 
-	val, err := encodeXML(doc, mapping)
+	str, err := encodeXML(doc, mapping)
 	if err != nil {
-		fmt.Printf("err: %s\n", err)
+		fmt.Printf("err := %v\n", err)
+		os.Exit(1)
 	}
 
-	fmt.Printf("encoded: %s\n", val)
+	fmt.Println(str)
 }
-
-var (
-	XMLStartElement    = "<"
-	XMLStartEndElement = "</"
-	XMLEndElement      = ">"
-	EqualSign          = "="
-	Slash              = "/"
-	Quote              = "\""
-)
 
 func encodeXML(value string, mapping map[string]int) (string, error) {
-	// parse element
-	// parse value
+	l := newLexer(value)
+	go l.lex()
 
-	sr := strings.NewReader(value)
-	sb := &strings.Builder{}
+	result := strings.Builder{}
+	var i int
+	for item := range l.items {
+		var s string
 
-	err := encode(sr, sb, mapping)
-	if errors.Is(err, io.EOF) {
-		return sb.String(), nil
+		switch item.typ {
+		case itemStartElement:
+			s = strconv.Itoa(mapping[item.val])
+		case itemEndElement, itemEndOfStartElement:
+			s = "0"
+		case itemValue:
+			s = item.val
+		case itemAttribute:
+			attr := strings.Split(item.val, "=")
+			key := strconv.Itoa(mapping[attr[0]])
+			val := strings.Trim(attr[1], "\"'")
+
+			s = key + " " + val
+		}
+
+		if i == 1 {
+			_, _ = result.WriteString(" ")
+		}
+
+		_, _ = result.WriteString(s)
+		i = 1
 	}
-	if err != nil {
-		return "", err
-	}
 
-	return sb.String(), nil
+	return result.String(), nil
 }
 
-func encode(sr *strings.Reader, sb *strings.Builder, mapping map[string]int) error {
-	r, _, err := sr.ReadRune()
-	if err != nil {
-		return fmt.Errorf("ReadRune: %w", err)
+type itemType int
+
+func (i itemType) String() string {
+	switch i {
+	case itemStartElement:
+		return "Element"
+	case itemEndElement:
+		return "Close Element"
+	case itemValue:
+		return "Value"
+	case itemAttribute:
+		return "Attribute"
+	case itemErr:
+		return "Error"
+	case itemEndOfStartElement:
+		return "GreaterThanSign"
+	default:
+		return fmt.Sprintf("unknown item type: %d", i)
+	}
+}
+
+const (
+	itemStartElement itemType = iota
+	itemEndOfStartElement
+	itemEndElement
+	itemAttribute
+	itemValue
+	// TODO: Define helpful err messages
+	itemErr
+
+	eof rune = -1
+)
+
+type item struct {
+	typ itemType
+	val string
+}
+
+// TODO: Ignore multiple spaces/newlines
+type lexer struct {
+	text  string
+	width int
+	start int
+	pos   int
+	items chan item
+}
+
+func newLexer(text string) *lexer {
+	return &lexer{
+		text:  text,
+		items: make(chan item),
+	}
+}
+
+func (l *lexer) emit(typ itemType) {
+	l.items <- item{
+		typ: typ,
+		val: l.text[l.start:l.pos],
+	}
+	l.start = l.pos
+
+	if typ == itemErr {
+		close(l.items)
+	}
+}
+
+func (l *lexer) peek() rune {
+	r := l.next()
+	if r != eof {
+		l.backup()
 	}
 
-	if unicode.IsSpace(r) {
-		return encode(sr, sb, mapping)
+	return r
+}
+
+func (l *lexer) backup() {
+	l.pos -= l.width
+}
+
+func (l *lexer) next() rune {
+	if l.pos >= len(l.text) {
+		l.width = 0
+		return eof
 	}
 
-	if string(r) == XMLStartElement {
-		r, _, err := sr.ReadRune()
-		if err != nil {
-			return fmt.Errorf("ReadRune: %w", err)
-		}
+	r, size := utf8.DecodeRuneInString(l.text[l.pos:])
+	if r == utf8.RuneError {
+		l.emit(itemErr)
+		return r
+	}
 
-		if string(r) == Slash {
-			err := exhaustEndElement(sr)
-			if err != nil {
-				return fmt.Errorf("exhaustEndElement: %w", err)
-			}
+	l.pos += size
+	l.width = size
+	return r
+}
 
-			_, err = sb.WriteString("0")
-			if err != nil {
-				return fmt.Errorf("writing %q: %w", " 0", err)
-			}
+func (l *lexer) consumeWhiteSpaces() {
+	for unicode.IsSpace(l.peek()) {
+		l.next()
+	}
 
-			// TODO: If traling space available we'll append " " to the end of the element
-			_, _, err = sr.ReadRune()
-			if errors.Is(err, io.EOF) {
-				return nil
-			} else {
-				err := sr.UnreadRune()
-				if err != nil {
-					return fmt.Errorf("UnreadRune: %w", err)
-				}
-				_, err = sb.WriteString(" ")
-				if err != nil {
-					return fmt.Errorf("writing %q: %w", " 0", err)
-				}
-			}
+	l.start = l.pos
+}
 
-		} else {
-			err := sr.UnreadRune()
-			if err != nil {
-				return fmt.Errorf("UnreadRune: %w", err)
-			}
+type stateFn func(l *lexer) stateFn
 
-			el, hasAttrs, err := parseStartElement(sr)
-			if err != nil {
-				return fmt.Errorf("parseStartElement: %w", err)
-			}
+// Initiate the lexing process
+func (l *lexer) lex() {
+	l.consumeWhiteSpaces()
+	for state := lexLessThanSign(l); state != nil; {
+		state = state(l)
+	}
+}
 
-			_, err = sb.WriteString(strconv.Itoa(mapping[el]))
-			if err != nil {
-				return fmt.Errorf("writing %q: %w", el, err)
-			}
+func lexLessThanSign(l *lexer) stateFn {
+	r := l.next()
+	if r != '<' {
+		l.emit(itemErr)
+		return nil
+	}
 
-			for hasAttrs {
-				tag, value, attrs, err := parseAttribute(sr)
-				if err != nil {
-					return fmt.Errorf("parsing attribute: %w", err)
-				}
+	l.start = l.pos
+	return lexStartElement
+}
 
-				_, err = sb.WriteString(" " + strconv.Itoa(mapping[tag]) + " " + value)
-				if err != nil {
-					return fmt.Errorf("writing %q: %w", tag+" "+value, err)
-				}
-
-				hasAttrs = attrs
-			}
-
-			_, err = sb.WriteString(" 0 ")
-			if err != nil {
-				return fmt.Errorf("writing %q: %w", " 0", err)
-			}
-		}
+func lexGreaterThanSign(l *lexer) stateFn {
+	l.start = l.pos
+	r := l.next()
+	if r != '>' {
+		l.emit(itemErr)
+		return nil
 	} else {
-		err := sr.UnreadRune()
-		if err != nil {
-			return fmt.Errorf("UnreadRune: %w", err)
-		}
-
-		value, err := parseValue(sr)
-		if err != nil {
-			return fmt.Errorf("parsing value: %w", err)
-		}
-
-		_, err = sb.WriteString(value + " 0 ")
-		if err != nil {
-			return fmt.Errorf("writing %q: %w", value+" 0", err)
-		}
-
-		err = exhaustEndElement(sr)
-		if err != nil {
-			return fmt.Errorf("exhaustEndElement: %w", err)
-		}
+		l.emit(itemEndOfStartElement)
 	}
 
-	return encode(sr, sb, mapping)
+	closeIdx := strings.Index(l.text[l.pos:], "</")
+	openIdx := strings.Index(l.text[l.pos:], "<")
+	if openIdx < closeIdx {
+		l.consumeWhiteSpaces()
+		return lexLessThanSign
+	}
+
+	return lexInsideElement
 }
 
-func parseValue(sr *strings.Reader) (string, error) {
-	value := strings.Builder{}
-
-	for {
-		r, _, err := sr.ReadRune()
-		if err != nil {
-			return "", err
-		}
-
-		if string(r) == XMLStartElement {
-			r, _, err := sr.ReadRune()
-			if err != nil {
-				return "", err
-			}
-			if string(r) == Slash {
-				break
-			}
-
-			err = sr.UnreadRune()
-			if err != nil {
-				return "", err
-			}
-		}
-
-		_, err = value.WriteRune(r)
-		if err != nil {
-			return "", err
-		}
+func lexStartElement(l *lexer) stateFn {
+	for unicode.IsLetter(l.peek()) {
+		l.next()
 	}
 
-	return value.String(), nil
+	l.emit(itemStartElement)
+
+	l.consumeWhiteSpaces()
+	r := l.peek()
+	if unicode.IsLetter(r) {
+		return lexAttributes
+	}
+
+	return lexGreaterThanSign
 }
 
-func parseAttribute(sr *strings.Reader) (string, string, bool, error) {
-	var tag, value = strings.Builder{}, strings.Builder{}
-	var hasAttrs bool
-
-	for {
-		r, _, err := sr.ReadRune()
-		if err != nil {
-			return "", "", false, err
+func lexAttributes(l *lexer) stateFn {
+	l.consumeWhiteSpaces()
+	for l.peek() != '>' {
+		eqIdx := strings.IndexRune(l.text[l.pos:], '=')
+		if eqIdx == -1 {
+			l.emit(itemErr)
+			return nil
+		}
+		l.pos += eqIdx + 1
+		// TODO: check for out of range
+		quote := l.next()
+		if quote != '\'' && quote != '"' {
+			l.emit(itemErr)
+			return nil
 		}
 
-		if string(r) == EqualSign {
-			break
-		}
+		nextQuoteIdx := strings.IndexRune(l.text[l.pos:], quote)
+		l.pos = l.pos + nextQuoteIdx + 1
+		l.emit(itemAttribute)
 
-		_, err = tag.WriteRune(r)
-		if err != nil {
-			return "", "", false, err
-		}
+		l.consumeWhiteSpaces()
 	}
 
-	for {
-		r, _, err := sr.ReadRune()
-		if err != nil {
-			return "", "", false, err
-		}
-
-		if string(r) == Quote {
-			continue
-		}
-
-		if string(r) == XMLEndElement {
-			break
-		}
-		if unicode.IsSpace(r) {
-			hasAttrs = true
-			break
-		}
-
-		_, err = value.WriteRune(r)
-		if err != nil {
-			return "", "", false, err
-		}
-	}
-
-	return tag.String(), value.String(), hasAttrs, nil
+	return lexGreaterThanSign
 }
 
-func parseStartElement(sr *strings.Reader) (string, bool, error) {
-	sb := strings.Builder{}
-	var hasAttrs bool
-	for {
-		r, _, err := sr.ReadRune()
-		if err != nil {
-			return "", false, err
-		}
-
-		// start element could have attributes
-		if unicode.IsSpace(r) {
-			hasAttrs = true
-			break
-		}
-		if string(r) == XMLEndElement {
-			break
-		}
-
-		_, err = sb.WriteRune(r)
-		if err != nil {
-			return "", false, err
-		}
+func lexInsideElement(l *lexer) stateFn {
+	idx := strings.Index(l.text[l.pos:], "</")
+	if idx == -1 {
+		l.emit(itemErr)
+		return nil
 	}
+	l.start = l.pos
+	l.pos = l.start + idx
+	l.emit(itemValue)
 
-	return sb.String(), hasAttrs, nil
+	// skip / in </
+	l.start = l.pos + 2
+	l.pos = l.start
+	return insideEndElement
 }
 
-func exhaustEndElement(sr *strings.Reader) error {
-	for {
-		r, _, err := sr.ReadRune()
-		if err != nil {
-			return err
-		}
-
-		if string(r) == XMLEndElement {
-			break
-		}
+func insideEndElement(l *lexer) stateFn {
+	idx := strings.IndexRune(l.text[l.pos:], '>')
+	if idx == -1 {
+		l.emit(itemErr)
+		return nil
 	}
 
-	return nil
+	l.pos += idx
+	l.emit(itemEndElement)
+
+	l.next()
+	l.start = l.pos
+	l.consumeWhiteSpaces()
+
+	// open xml element or closing another one is expected
+	r := l.next()
+	if r == eof {
+		close(l.items)
+		return nil
+	}
+	if r != '<' {
+		l.emit(itemErr)
+		return nil
+	}
+	if l.peek() == '/' {
+		l.next()
+		l.start = l.pos
+		return insideEndElement
+	}
+
+	l.start = l.pos
+	return lexStartElement
 }
