b64e2186190f75122cb23b2641a89396a0537ea7
Ivan Ivanov <ivan.ivanov@ft.com>
2023-04-07 19:11:33 +0300

    sicp: 2.2.2 Hierarchical Structures - (list 1 (list 2 (list 3 4)))

---
+++ sicp/ch02/24.scm
@@ -0,0 +1,35 @@
+;; Exercise 2.24. Suppose we evaluate the expression:
+;; (list 1 (list 2 (list 3 4))).
+;; Give the result printed by the interpreter, the corresponding
+;; box-and-pointer structure, and the interpretation of this as a
+;; tree (as in figure 2.6).
+
+;; The result printed by the interpreter should be:
+;; (1 (2 (3 4)))
+
+;; The box-and-pointer structure should be(my way is not exactly
+;; box-and-pointer):
+;; [1, [2, [3, 4]]]
+;; This is a list of two elements, the first element is 1, the second
+;; element is another list.
+
+(define t (list 1 (list 2 (list 3 4))))
+
+(length t)
+
+;; The tree should look like this, with 4 nodes.
+;; 
+;;     /\
+;;    1  \
+;;       /\ 
+;;      2  \
+;;         /\
+;;        3  4
+
+(define (count-leaves x)
+  (cond ((null? x) 0)  
+        ((not (pair? x)) 1)
+        (else (+ (count-leaves (car x))
+                 (count-leaves (cdr x))))))
+
+(count-leaves t)
