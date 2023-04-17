74ed50fe81274322a568f2793f1471ddc65a4104
Ivan Ivanov <ivan.ivanov@ft.com>
2023-04-17 20:22:41 +0300

    sicp: 2.2.3 Lists as Conventional Interfaces - accumulate-n

---
+++ sicp/ch02/36.scm
@@ -0,0 +1,27 @@
+;; Exercise 2.36. The procedure accumulate-n is similar to accumulate
+;; except that it takes as its third argument a sequence of sequences,
+;; which are all assumed to have the same number of elements. It applies
+;; the designated accumulation procedure to combine all the first
+;; elements of the sequences, all the second elements of the sequences,
+;; and so on, and returns a sequence of the results. For instance, if s
+;; is a sequence containing four sequences, ((1 2 3) (4 5 6) (7 8 9) (10
+;; 11 12)), then the value of (accumulate-n + 0 s) should be the sequence
+;; (22 26 30). Fill in the missing expressions in the following
+;; definition of accumulate-n:
+
+(define (accumulate op init sequence)
+  (if (null? sequence)
+      init
+      (op (car sequence)
+          (accumulate op init (cdr sequence)))))
+
+(define (accumulate-n op init seqs)
+  (if (or (null? seqs)
+          (null? (car seqs)))
+      ()
+      (cons (accumulate op init (map car seqs))
+            (accumulate-n op init (map cdr seqs)))))
+
+(define seqs '((1 2 3) (4 5 6) (7 8 9) (10 11 12)))
+
+(accumulate-n + 0 seqs)
