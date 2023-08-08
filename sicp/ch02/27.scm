c3c99478f9358b87e91862294be0fb53db2f7e19
Ivan Ivanov <ruski9606@gmail.com>
2023-08-08 23:37:02 +0300

    sicp: 2.2.2 Hierarchical Structures - fix deep-reverse

--- sicp/ch02/27.scm
+++ sicp/ch02/27.scm
@@ -5,22 +5,20 @@
  
 (define x (list (list 1 2) (list 3 4)))
 (define y (list 1 (list 2 3) 4 (list 5 6)))
-
-(define (reverse l)
-  (if (or (null? l) (= (length l) 1))
-      l
-      (append (reverse (cdr l))
-              (list (car l)))))
+(define z (list 1 (list 2 3) 4 (list 5 6 (list 7 (list 8 9)))))
 
 (reverse x)
+(reverse y)
+(reverse z)
 
 (define (deep-reverse l)
   (cond ((null? l) l)
         ((list? (car l))
          (append (deep-reverse (cdr l))
-               (list (reverse (car l)))))
+                 (list (deep-reverse (car l)))))
         (else (append (deep-reverse (cdr l))
                       (list (car l))))))
 
 (deep-reverse x)
 (deep-reverse y)
+(deep-reverse z)
