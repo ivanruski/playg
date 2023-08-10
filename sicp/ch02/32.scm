86b54e4bb1478824a18f9ea48331a3bc71ea13c4
Ivan Ivanov <ruski9606@gmail.com>
2023-08-11 00:01:36 +0300

    sicp: 2.2.2 Hierarchical Structures - subsets
    Trying to provide clearer explanation of how subsets work.

--- sicp/ch02/32.scm
+++ sicp/ch02/32.scm
@@ -16,14 +16,23 @@
 (subsets (list 1 2 3))
 
 ;; Why it works ?
-;; rest is a list of lists. For each of the sublists of rest we append
-;; the first element of s, like below:
-(map (lambda (x) (append '(1) x)) '(() (2) (3) (2 3)))
-;; We will generate all of the subsets for the remaining of the list
-;; and we will create new list where some of the elements will be all
-;; of the subsets for the remaining of the lists and the other elements
-;; will be formed by appending the first element of s to those subsets.
-;; e.g.
-(append '(() (2) (3) (2 3))
-        (map (lambda (x) (append '(1) x)) '(() (2) (3) (2 3))))
+;; The idea of the function is to find all of the subsets for (cdr s) and then
+;; create new sequence consisting of all of those subsets as well as appending
+;; (list (car s)) to copies of those subsets.
+;;
+;; For example:
+;; input: ()
+;; output: (())
+;;
+;; input: (1)
+;; output: (() (1))
+;;
+;; In the first invocation of subsets s is (1) and rest is (()) and in the let
+;; we create the result by appending (()) to ((1)), giving ((), (1)).
+;;
+;; input: (1 2)
+;; output: (() (2) (1) (1 2))
+;; In the first invocation of subsets s i (1 2) and rest is (() (2)). When we
+;; append (list (car s)) to () and (2) we get ((1) (1 2)) which we append to (()
+;; (2)) to get the result.
 
