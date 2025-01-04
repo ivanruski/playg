33ab2627816c56db7816cfe217d52a7130988eac
Ivan Ivanov <ruski9606@gmail.com>
2025-01-04 11:13:20 +0200

    sicp: 2.1.4 Extended Exercise: Interval Arithmetic - two diff worlds

---
+++ sicp/ch02/16.scm
@@ -0,0 +1,9 @@
+;; Exercise 2.16. Explain, in general, why equivalent algebraic expressions may
+;; lead to different answers. Can you devise an interval-arithmetic package that
+;; does not have this shortcoming, or is this task impossible? (Warning: This
+;; problem is very difficult.)
+
+;; The short and layman answer that I can give is that interval arithmetic is
+;; not the same as integer arithmetic(something I allued to in ex. 14 & 15).
+;;
+;; Skipping the second part.
