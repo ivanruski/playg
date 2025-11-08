c8eff88b2a6cb120d0438013186628d1c091e559
Ivan Ivanov <ruski9606@gmail.com>
2025-11-08 13:05:05 +0200

    sicp: 3.3.1 Mutable List Structure - make-cycle

---
+++ sicp/ch03/13.scm
@@ -0,0 +1,25 @@
+;; Exercise 3.13. Consider the following make-cycle procedure, which uses the
+;; last-pair procedure defined in exercise 3.12:
+
+(define (last-pair x)
+  (if (null? (cdr x))
+      x
+      (last-pair (cdr x))))
+
+(define (make-cycle x)
+  (set-cdr! (last-pair x) x)
+  x)
+
+;; Draw a box-and-pointer diagram that shows the structure z created by
+
+(define z (make-cycle (list 'a 'b 'c)))
+
+;;         +-----------------------------------+
+;;         v                                   |
+;;       +---+---+    +---+---+     +---+---+  |
+;; z --> | . | -----> |   | ----->  |   | -----+
+;;       +-|-+---+    +-|-+---+     +-|-+---+
+;;         v            v             v
+;;       +---+        +---+         +---+
+;;       | a |        | b |         | c |
+;;       +---+        +---+         +---+
