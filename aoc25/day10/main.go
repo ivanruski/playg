526d834a3d6fbf106e0f513304fa36cfa8b168cb
Ivan Ivanov <ruski9606@gmail.com>
2025-12-22 11:42:31 +0200

    aoc25: Day 10: Factory

--- aoc25/day10/main.go
+++ aoc25/day10/main.go
@@ -11,32 +11,41 @@ import (
 	"strings"
 )
 
+// https://adventofcode.com/2025/day/10
+//
+// HUGE THANKS to https://www.reddit.com/r/adventofcode/comments/1pk87hl/2025_day_10_part_2_bifurcate_your_way_to_victory/
+// for part 2. I wouldn't have done it otherwise.
 func main() {
 	diagrams, _ := readInput()
 
 	fmt.Println(analyzeDiagrams(diagrams))
+	fmt.Println(configureJoltages(diagrams))
 }
 
 func analyzeDiagrams(diagrams []diagram) int {
 	var sum int
 	for _, d := range diagrams {
-		sum += analyzeDiagram(d)
-	}
+		validCombinations := analyzeDiagram(d.lightsTarget, d.buttonWirings)
+		slices.SortFunc(validCombinations, func(a, b [][]int) int {
+			return len(a) - len(b)
+		})
 
+		sum += len(validCombinations[0])
+	}
 	return sum
 }
 
-func analyzeDiagram(diagram diagram) int {
-	target := strings.Builder{}
-	for _, c := range diagram.lightsTarget {
-		target.WriteString(strconv.Itoa(c))
+func analyzeDiagram(target []int, pressings [][]int) [][][]int {
+	t := strings.Builder{}
+	for _, c := range target {
+		t.WriteString(strconv.Itoa(c))
 	}
 
-	targetNum, _ := strconv.ParseInt(target.String(), 2, 64)
+	targetNum, _ := strconv.ParseInt(t.String(), 2, 64)
 
 	buttonWirings := []int{}
-	for _, bw := range diagram.buttonWirings {
-		bits := make([]int, target.Len())
+	for _, bw := range pressings {
+		bits := make([]int, t.Len())
 		for _, n := range bw {
 			bits[n] = 1
 		}
@@ -50,85 +59,148 @@ func analyzeDiagram(diagram diagram) int {
 		buttonWirings = append(buttonWirings, int(num))
 	}
 
-	type node struct {
-		num       int
-		vertecies []*node
-	}
-
-	ln := math.Pow(2, float64(target.Len()))
-	nodes := make([][]int, int(ln))
-	for _, bw := range buttonWirings {
-		nodes[bw] = make([]int, 0, 4)
-	}
-
-	for {
-		nodesAdded := 0
-
-		for node, vertices := range nodes {
-			if cap(vertices) == 0 {
-				continue
-			}
-
-			vs := vertices
-			for _, bw := range buttonWirings {
-				newNode := node ^ bw
-				if nodes[newNode] == nil {
-					nodes[newNode] = make([]int, 0, 4)
-					nodesAdded++
-				}
-				vs = append(vs, newNode)
-			}
-
-			slices.Sort(vs)
-			nodes[node] = slices.Compact(vs)
+	min := math.MaxInt32
+	pressingCombinations := powerset(pressings)
+	result := [][][]int{}
+	for _, pressingCombination := range pressingCombinations {
+		num := 0
+		for _, buttonPresses := range pressingCombination {
+			num ^= combinePressesToBitPattern(buttonPresses, t.Len())
 		}
 
-		if nodesAdded == 0 {
-			break
+		if num == int(targetNum) {
+			result = append(result, pressingCombination)
+			if len(pressingCombination) < min {
+				min = len(pressingCombination)
+			}
 		}
 	}
 
-	min := math.MaxInt
-	for _, bw := range buttonWirings {
-		visited := make([]bool, len(nodes))
-		memo := make([]int, len(nodes))
-		path := 1 + findShortestPathToNum(bw, int(targetNum), nodes, visited, memo)
-		if min > path {
-			min = path
+	return result
+}
+
+func combinePressesToBitPattern(bw []int, x int) int {
+	bits := make([]int, x)
+	for _, n := range bw {
+		bits[n] = 1
+	}
+
+	pattern := strings.Builder{}
+	for _, n := range bits {
+		pattern.WriteString(strconv.Itoa(n))
+	}
+
+	num, _ := strconv.ParseInt(pattern.String(), 2, 64)
+	return int(num)
+}
+
+func powerset(buttonWirings [][]int) [][][]int {
+	if len(buttonWirings) == 0 {
+		return [][][]int{{}}
+	}
+
+	first := buttonWirings[0]
+	rest := buttonWirings[1:]
+
+	ps := powerset(rest)
+	res := make([][][]int, 0, len(ps)*2)
+
+	for _, subset := range ps {
+		res = append(res, subset)
+
+		s := make([][]int, len(subset), len(subset)+1)
+		copy(s, subset)
+		s = append(s, first)
+		res = append(res, s)
+	}
+
+	return res
+}
+
+func configureJoltages(diagrams []diagram) int {
+	sum := 0
+	for _, d := range diagrams {
+		res := configureDiagramJoltages(d.joltageRequirements, d.buttonWirings, map[int][][][]int{})
+		sum += res
+	}
+
+	return sum
+}
+
+func configureDiagramJoltages(counters []int, combinations [][]int, memo map[int][][][]int) int {
+	allZeroes := true
+	for _, d := range counters {
+		if d > 0 {
+			allZeroes = false
+		}
+		if d < 0 {
+			return math.MaxInt32
+		}
+	}
+
+	if allZeroes {
+		return 0
+	}
+
+	target := []int{}
+	for _, n := range counters {
+		target = append(target, n%2)
+	}
+
+	min := math.MaxInt32
+	var validCombinations [][][]int
+	if vc, ok := memo[key(target)]; ok {
+		validCombinations = vc
+	} else {
+		validCombinations = analyzeDiagram(target, combinations)
+		memo[key(target)] = validCombinations
+	}
+
+	for _, pattern := range validCombinations {
+		joltages := make([]int, len(counters))
+		copy(joltages, counters)
+		for _, buttonPressings := range pattern {
+			for _, b := range buttonPressings {
+				joltages[b]--
+			}
+		}
+
+		allEven := true
+		for _, j := range joltages {
+			if j%2 != 0 {
+				allEven = false
+			}
+		}
+		if allEven {
+			for i := range joltages {
+				joltages[i] /= 2
+			}
+		}
+
+		cnt := configureDiagramJoltages(joltages, combinations, memo)
+		if cnt != math.MaxInt32 {
+			if allEven {
+				cnt = cnt * 2
+			}
+			cnt = cnt + len(pattern)
+		}
+		if min > cnt {
+			min = cnt
 		}
 	}
 
 	return min
 }
 
-func findShortestPathToNum(start, target int, nodes [][]int, visited []bool, memo []int) int {
-	if visited[start] {
-		return math.MaxInt32
+func key(s []int) int {
+	mul := int(math.Pow(10, float64(len(s)-1)))
+	sum := 0
+	for i := len(s) - 1; i >= 0; i-- {
+		sum += mul * s[i]
+		mul /= 10
 	}
 
-	if start == target {
-		return 0
-	}
-
-	visited[start] = true
-	min := math.MaxInt32
-
-	for _, v := range nodes[start] {
-		var path int
-		if memo[v] != 0 {
-			path = 1 + memo[v]
-		} else {
-			path = 1 + findShortestPathToNum(v, target, nodes, visited, memo)
-		}
-		if min > path {
-			min = path
-		}
-	}
-
-	memo[start] = min
-	visited[start] = false
-
-	return min
+	return sum
 }
 
 type diagram struct {
